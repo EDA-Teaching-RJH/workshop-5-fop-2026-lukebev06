81f413656cdcedb80ac2e82f5a36c95968121ffa
lukebev06 <lmb73@kent.ac.uk>
2026-02-02 20:07:29 +0000

    part2

--- coffee.py
+++ coffee.py
@@ -1,6 +1,7 @@
-#50p 20p 10p 5p
+
 
 def coffee():
+ total = 0
  needed_pay = int(input("what is the total amount need to pay in pennies? "))
  while needed_pay != 0 :
     coin = int(input("what is the first coin you are inputting?"))
@@ -9,15 +10,17 @@ def coffee():
       needed_pay = needed_pay - coin
     elif coin == 20:
         total = total + coin
-        needed_pay = total + coin
+        needed_pay =  needed_pay - coin
     elif coin == 10:
         total = total + coin
-        needed_pay = total + coin
+        needed_pay = needed_pay - coin
     elif coin == 5:
         total = total + coin
-        needed_pay = total + coin
+        needed_pay = needed_pay - coin
     else:
        print("that isnt a valid coin amount")
-    if total%75 == 0 :
+    if total >= 75:
        print(f"you have a total of {needed_pay}p left to pay")
-
+    
+    
+coffee()
