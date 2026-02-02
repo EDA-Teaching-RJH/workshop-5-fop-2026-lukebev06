ee8462db9f5c68428c06ecba26dbfa8e0d4257b3
lukebev06 <lmb73@kent.ac.uk>
2026-02-02 19:36:50 +0000

    done

--- camel.py
+++ camel.py
@@ -1,1 +1,13 @@
-# Enter your code here
+def camel_case(): 
+
+ string1 = input("input a string in camel case e.g youTube : ")
+ string2 = ""
+
+ for character in string1:
+     if character.isupper():
+         string2 = string2 + "_"+character.lower()
+     else:
+         string2 = string2 + character
+ print(string2)
+
+camel_case()
