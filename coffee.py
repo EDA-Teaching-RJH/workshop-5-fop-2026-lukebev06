b1f6c3b84821878bf84eea5b43e89930a6b2f958
lukebev06 <lmb73@kent.ac.uk>
2026-02-02 20:30:20 +0000

    part3 nearly done

--- coffee.py
+++ coffee.py
@@ -1,26 +1,35 @@
+#statement of requirements
 
+#       any valid integar that is a multiple of 5 above o
+#       prevent invalid totals before going into while
+#       remove errors in coin values before it goes into the if statement
+p
 
 def coffee():
- total = 0
- needed_pay = int(input("what is the total amount need to pay in pennies? "))
- while needed_pay != 0 :
-    coin = int(input("what is the first coin you are inputting?"))
-    if coin ==50:
-      total = total + coin
-      needed_pay = needed_pay - coin
-    elif coin == 20:
-        total = total + coin
-        needed_pay =  needed_pay - coin
-    elif coin == 10:
-        total = total + coin
-        needed_pay = needed_pay - coin
-    elif coin == 5:
-        total = total + coin
-        needed_pay = needed_pay - coin
-    else:
-       print("that isnt a valid coin amount")
-    if total >= 75:
-       print(f"you have a total of {needed_pay}p left to pay")
+ valid_coins = [50,20,10,5]
+
+ price = int(input("how much is owed? "))
+ total = 0  
+
+ 
+ while total < price :
+    try:
+        coin = int(input("enter a valid coin: "))
+
+        if coin in valid_coins:
+           total = total + coin
+
+    except ValueError:
+
+        print("enter a valid input.")
     
+    amount_over = total - price
+ print(f"Amount owed: {amount_over}p")
+
+    
+    
+
+
+
     
 coffee()
